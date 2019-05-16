3ec35d90b9ad0e8f8cc4f42541b3689c77c9d0d5
yumetsuki <540160575@qq.com>
2019-05-16 10:45:38 +0800

    fix api

--- src/main/kotlin/cn/yumetsuki/vertx_ktx_sql/sql/SQL.kt
+++ src/main/kotlin/cn/yumetsuki/vertx_ktx_sql/sql/SQL.kt
@@ -23,97 +23,97 @@ class SQL private constructor(private val builder: StringBuilder) {
          * Concat "select" and a string
          * @param s a string after select
          * */
-        fun select(s: String): StringBuilder = builder.append("select $s ")
+        fun select(s: String): SQLBuilder = builder.append("select $s ").let { this }
 
         /**
          * Concat "from" and a string
          * @param s a string after from
          * */
-        fun from(s: String): StringBuilder = builder.append("from $s ")
+        fun from(s: String): SQLBuilder = builder.append("from $s ").let { this }
 
         /**
          * Concat "where" and a string
          * @param s a string after where
          * */
-        fun where(s: String): StringBuilder = builder.append("where $s ")
+        fun where(s: String): SQLBuilder = builder.append("where $s ").let { this }
 
         /**
          * Concat "insert_into" and a string
          * @param s a string after insert_into
          * */
-        fun insert_into(s: String): StringBuilder = builder.append("insert into $s ")
+        fun insert_into(s: String): SQLBuilder = builder.append("insert into $s ").let { this }
 
         /**
          * Concat "value" and a string
          * @param list a string list after value and in ()
          * */
-        fun value(list: List<String>): StringBuilder = builder.append("value(${list.joinToString(",")}) ")
+        fun value(list: List<String>): SQLBuilder = builder.append("value(${list.joinToString(",")}) ").let { this }
 
         /**
          * Concat "values" and a string
          * @param list a string list after values and in ()
          * */
-        fun values(list: List<String>): StringBuilder = builder.append("values(${list.joinToString(",")}) ")
+        fun values(list: List<String>): SQLBuilder = builder.append("values(${list.joinToString(",")}) ").let { this }
 
         /**
          * Put a list into ()
          * @param list a string list
          * */
-        fun parentheses(list: List<String>): StringBuilder = builder.append("(${list.joinToString(",")}) ")
+        fun parentheses(list: List<String>): SQLBuilder = builder.append("(${list.joinToString(",")}) ").let { this }
 
         /**
          * Concat "value" and a string
          * @param sequence a string sequence after value and in ()
          * */
-        fun value(sequence: Sequence<String>): StringBuilder = builder.append("value(${sequence.joinToString(",")}) ")
+        fun value(sequence: Sequence<String>): SQLBuilder = builder.append("value(${sequence.joinToString(",")}) ").let { this }
 
         /**
          * Concat "values" and a string
          * @param sequence a string sequence a string list after values and in ()
          * */
-        fun values(sequence: Sequence<String>): StringBuilder = builder.append("values(${sequence.joinToString(",")}) ")
+        fun values(sequence: Sequence<String>): SQLBuilder = builder.append("values(${sequence.joinToString(",")}) ").let { this }
 
         /**
          * Put a list into ()
          * @param sequence a string sequence
          * */
-        fun parentheses(sequence: Sequence<String>): StringBuilder = builder.append("(${sequence.joinToString(",")}) ")
+        fun parentheses(sequence: Sequence<String>): SQLBuilder = builder.append("(${sequence.joinToString(",")}) ").let { this }
 
         /**
          * Concat "update" and a string
          * @param s a string after update
          * */
-        fun update(s: String): StringBuilder = builder.append("update $s ")
+        fun update(s: String): SQLBuilder = builder.append("update $s ").let { this }
 
         /**
          * Concat "set" and a string
          * @param s a string after set
          * */
-        fun set(s: String): StringBuilder = builder.append("set $s ")
+        fun set(s: String): SQLBuilder = builder.append("set $s ").let { this }
 
         /**
          * Concat "equals" and a string
          * @param s a string after equals
          * */
-        fun eql(s: String): StringBuilder = builder.append("=$s ")
+        fun eql(s: String): SQLBuilder = builder.append("=$s ").let { this }
 
         /**
          * Concat "inner join" and a string
          * @param s a string after inner join
          * */
-        fun inner_join(s: String): StringBuilder = builder.append("inner join $s ")
+        fun inner_join(s: String): SQLBuilder = builder.append("inner join $s ").let { this }
 
         /**
          * Concat "left join" and a string
          * @param s a string after left join
          * */
-        fun left_join(s: String): StringBuilder = builder.append("left join $s ")
+        fun left_join(s: String): SQLBuilder = builder.append("left join $s ").let { this }
 
         /**
          * Concat "right join" and a string
          * @param s a string after right join
          * */
-        fun right_join(s: String): StringBuilder = builder.append("right join $s ")
+        fun right_join(s: String): SQLBuilder = builder.append("right join $s ").let { this }
 
         /**
          * Build SQL with StringBuilder
@@ -124,7 +124,7 @@ class SQL private constructor(private val builder: StringBuilder) {
          * Concat a string
          * @param s a string
          * */
-        fun concat(s: String): StringBuilder = builder.append("$s ")
+        fun concat(s: String): SQLBuilder = builder.append("$s ").let { this }
 
         /**
          * Concat this builder and another
--- src/main/kotlin/cn/yumetsuki/vertx_ktx_sql/sql/SQLBuilderExt.kt
+++ src/main/kotlin/cn/yumetsuki/vertx_ktx_sql/sql/SQLBuilderExt.kt
@@ -4,39 +4,39 @@ suspend fun sql(handler: suspend SQL.SQLBuilder.() -> SQL.SQLBuilder): SQL {
     return handler(SQL.SQLBuilder()).build()
 }
 
-infix fun SQL.SQLBuilder.select(s: String): SQL.SQLBuilder = select(s).let { this }
+infix fun SQL.SQLBuilder.select(s: String): SQL.SQLBuilder = select(s)
 
-infix fun SQL.SQLBuilder.from(s: String): SQL.SQLBuilder = from(s).let { this }
+infix fun SQL.SQLBuilder.from(s: String): SQL.SQLBuilder = from(s)
 
-infix fun SQL.SQLBuilder.where(s: String): SQL.SQLBuilder = where(s).let { this }
+infix fun SQL.SQLBuilder.where(s: String): SQL.SQLBuilder = where(s)
 
-infix fun SQL.SQLBuilder.insert_into(s: String): SQL.SQLBuilder = insert_into(s).let { this }
+infix fun SQL.SQLBuilder.insert_into(s: String): SQL.SQLBuilder = insert_into(s)
 
-infix fun SQL.SQLBuilder.value(list: List<String>): SQL.SQLBuilder = value(list).let { this }
+infix fun SQL.SQLBuilder.value(list: List<String>): SQL.SQLBuilder = value(list)
 
-infix fun SQL.SQLBuilder.values(list: List<String>): SQL.SQLBuilder = values(list).let { this }
+infix fun SQL.SQLBuilder.values(list: List<String>): SQL.SQLBuilder = values(list)
 
-infix fun SQL.SQLBuilder.value(sequence: Sequence<String>): SQL.SQLBuilder = value(sequence).let { this }
+infix fun SQL.SQLBuilder.value(sequence: Sequence<String>): SQL.SQLBuilder = value(sequence)
 
-infix fun SQL.SQLBuilder.values(sequence: Sequence<String>): SQL.SQLBuilder = values(sequence).let { this }
+infix fun SQL.SQLBuilder.values(sequence: Sequence<String>): SQL.SQLBuilder = values(sequence)
 
-infix fun SQL.SQLBuilder.parentheses(list: List<String>): SQL.SQLBuilder = parentheses(list).let { this }
+infix fun SQL.SQLBuilder.parentheses(list: List<String>): SQL.SQLBuilder = parentheses(list)
 
-infix fun SQL.SQLBuilder.parentheses(sequence: Sequence<String>): SQL.SQLBuilder = parentheses(sequence).let { this }
+infix fun SQL.SQLBuilder.parentheses(sequence: Sequence<String>): SQL.SQLBuilder = parentheses(sequence)
 
-infix fun SQL.SQLBuilder.update(s: String): SQL.SQLBuilder = update(s).let { this }
+infix fun SQL.SQLBuilder.update(s: String): SQL.SQLBuilder = update(s)
 
-infix fun SQL.SQLBuilder.set(s: String): SQL.SQLBuilder = set(s).let { this }
+infix fun SQL.SQLBuilder.set(s: String): SQL.SQLBuilder = set(s)
 
-infix fun SQL.SQLBuilder.eql(s: String): SQL.SQLBuilder = eql(s).let { this }
+infix fun SQL.SQLBuilder.eql(s: String): SQL.SQLBuilder = eql(s)
 
-infix fun SQL.SQLBuilder.inner_join(s: String): SQL.SQLBuilder = inner_join(s).let { this }
+infix fun SQL.SQLBuilder.inner_join(s: String): SQL.SQLBuilder = inner_join(s)
 
-infix fun SQL.SQLBuilder.left_join(s: String): SQL.SQLBuilder = left_join(s).let { this }
+infix fun SQL.SQLBuilder.left_join(s: String): SQL.SQLBuilder = left_join(s)
 
-infix fun SQL.SQLBuilder.right_join(s: String): SQL.SQLBuilder = right_join(s).let { this }
+infix fun SQL.SQLBuilder.right_join(s: String): SQL.SQLBuilder = right_join(s)
 
-infix fun SQL.SQLBuilder.concat(s: String): SQL.SQLBuilder = concat(s).let { this }
+infix fun SQL.SQLBuilder.concat(s: String): SQL.SQLBuilder = concat(s)
 
 
 
